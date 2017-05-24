435e57d46701eddd3b2eae6d8537b7b86b06c85f
Lucid Sunlight <lucidsunlight@yandex.ru>
2017-05-25 02:14:55 +0700

    Landing

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Landing from '@/components/Landing'
+import 'bulma'
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Landing',
+      component: Landing
+    }
+  ]
+})
