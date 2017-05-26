72add630b014c208e0b33dc8c59dc78af301008b
Lucid Sunlight <lucidsunlight@yandex.ru>
2017-05-26 07:48:59 +0700

    Change cmd list path

--- src/router/index.js
+++ src/router/index.js
@@ -18,7 +18,7 @@ export default new Router({
       component: Sigma
     },
     {
-      path: '/commands',
+      path: '/sigma/commands',
       name: 'Sigma - Command list',
       component: Commands
     }
